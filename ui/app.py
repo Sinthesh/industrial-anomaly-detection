23dd469b48eb44efc201046bb70b21ccd4cd0972
Sinthesh <sinthesh.s@gmail.com>
2026-03-05 20:29:12 +0530

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -5,23 +5,13 @@ import numpy as np
 import cv2
 from PIL import Image
 
-# Allow project imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from mcp_server.controller import process_inspection
 
-
-# -----------------------------
-# Page Title
-# -----------------------------
-
 st.title("Industrial Defect Detection System")
+
 st.write("Upload a product image and run anomaly inspection.")
-
-
-# -----------------------------
-# Upload Image
-# -----------------------------
 
 uploaded_file = st.file_uploader("Upload Image")
 
@@ -29,11 +19,6 @@ product = st.selectbox(
     "Select Product Type",
     ["bottle", "hazelnut", "metalnut", "pill"]
 )
-
-
-# -----------------------------
-# When Image Uploaded
-# -----------------------------
 
 if uploaded_file is not None:
 
@@ -46,11 +31,6 @@ if uploaded_file is not None:
 
     st.image(image, caption="Uploaded Image", width=350)
 
-
-    # -----------------------------
-    # Run Inspection Button
-    # -----------------------------
-
     if st.button("Run Inspection"):
 
         result = process_inspection(temp_path, product)
@@ -60,49 +40,20 @@ if uploaded_file is not None:
 
         st.write("## Inspection Result")
 
-        # -----------------------------
-        # Defect Status
-        # -----------------------------
-
         if score > 0.5:
             st.error(f"DEFECT DETECTED 🔴 | Score: {round(score,3)}")
         else:
             st.success(f"NORMAL PRODUCT 🟢 | Score: {round(score,3)}")
 
-
-        # -----------------------------
-        # Visualization Layout
-        # -----------------------------
-
         col1, col2, col3 = st.columns(3)
-
-
-        # -----------------------------
-        # Original Image
-        # -----------------------------
 
         with col1:
             st.subheader("Original")
             st.image(image, width=250)
 
-
-        # -----------------------------
-        # Heatmap
-        # -----------------------------
-
         with col2:
             st.subheader("Heatmap")
-
-            st.image(
-                heatmap,
-                caption="Anomaly Heatmap",
-                clamp=True
-            )
-
-
-        # -----------------------------
-        # Overlay Detection
-        # -----------------------------
+            st.image(heatmap, clamp=True)
 
         with col3:
 
@@ -125,12 +76,7 @@ if uploaded_file is not None:
                 0
             )
 
-            st.image(overlay, caption="Overlay Detection")
-
-
-        # -----------------------------
-        # Raw Result JSON
-        # -----------------------------
+            st.image(overlay)
 
         st.write("### Inspection Data")
         st.json(result)
