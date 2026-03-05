acc407d5e2d5766ca490a0173774a0ac27e0afc7
Sinthesh <sinthesh.s@gmail.com>
2026-03-05 06:50:59 +0530

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,11 +1,15 @@
 import streamlit as st
-import os
 import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 
 # Fix project imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from mcp_server.controller import process_inspection
+
 
 st.title("Industrial Defect Detection System")
 
@@ -25,11 +29,43 @@ if uploaded_file is not None:
     with open(temp_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    st.image(temp_path, caption="Uploaded Image", use_column_width=True)
+    image = Image.open(temp_path)
+
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Run Inspection"):
 
         result = process_inspection(temp_path, product)
 
-        st.write("### Inspection Result")
-        st.json(result)
+        score = result["score"]
+        heatmap = np.array(result["heatmap"])
+        runtime = result["runtime_seconds"]
+
+        st.write("## Inspection Result")
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write("### Anomaly Heatmap")
+
+            fig, ax = plt.subplots()
+            ax.imshow(heatmap, cmap="jet")
+            ax.axis("off")
+
+            st.pyplot(fig)
+
+        with col2:
+            st.write("### Overlay Detection")
+
+            fig2, ax2 = plt.subplots()
+
+            ax2.imshow(image.resize((224,224)))
+            ax2.imshow(heatmap, cmap="jet", alpha=0.5)
+            ax2.axis("off")
+
+            st.pyplot(fig2)
+
+        st.write("### Metrics")
+
+        st.metric("Anomaly Score", round(score,3))
+        st.metric("Runtime (seconds)", runtime)
